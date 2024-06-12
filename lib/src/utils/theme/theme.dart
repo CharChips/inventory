84356f71cf69763d7260ab92f74eac03f8ae1935
CharChips <sahoocharchit@gmail.com>
2024-06-13 00:42:29 +0530

    splashscreen and themes update

---
+++ lib/src/utils/theme/theme.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CAppTheme {
+  static ThemeData lightTheme = ThemeData(
+      brightness: Brightness.light,
+      textTheme: TextTheme(
+          headline2: GoogleFonts.montserrat(
+        color: Colors.black87,
+      )));
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+  );
+}
