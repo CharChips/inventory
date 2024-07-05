414b009d1dd815e1d1dc643b73b5fd02cbbf2f8e
CharChips <sahoocharchit@gmail.com>
2024-07-05 12:55:45 +0530

    modified profile

--- lib/src/features/authentication/screens/login/login_screen.dart
+++ lib/src/features/authentication/screens/login/login_screen.dart
@@ -17,39 +17,42 @@ class LoginScreen extends StatelessWidget {
             padding: const EdgeInsets.all(20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image(
                   image: const AssetImage(
                       "assets/images/isa-vesit-color-logo.png"),
-                  height: size.height * 0.2,
+                  height: size.height * 0.3,
                 ),
-                SizedBox(height: 15,),
+                SizedBox(
+                  height: 15,
+                ),
                 Text("Welcome Back",
                     style: Theme.of(context).textTheme.headlineMedium),
                 const LoginForm(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text("OR"),
-                    SizedBox(
-                      width: double.infinity,
-                      child: OutlinedButton.icon(
-                        icon: const Image(
-                          image: AssetImage(GoogleLogo),
-                          width: 20,
-                        ),
-                        onPressed: () {},
-                        label: const Text("Sign-In with Google"),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextButton(
-                        onPressed: () {},
-                        child: const Text("Already have an account"))
-                  ],
-                )
+                // Column(
+                //   crossAxisAlignment: CrossAxisAlignment.center,
+                //   children: [
+                //     const Text("OR"),
+                //     SizedBox(
+                //       width: double.infinity,
+                //       child: OutlinedButton.icon(
+                //         icon: const Image(
+                //           image: AssetImage(GoogleLogo),
+                //           width: 20,
+                //         ),
+                //         onPressed: () {},
+                //         label: const Text("Sign-In with Google"),
+                //       ),
+                //     ),
+                //     const SizedBox(
+                //       height: 10,
+                //     ),
+                //     TextButton(
+                //         onPressed: () {},
+                //         child: const Text("Already have an account"))
+                //   ],
+                // )
               ],
             ),
           ),
