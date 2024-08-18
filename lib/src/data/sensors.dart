d0105dfb265cc60356beb3ba26236c592533c59c
CharChips <sahoocharchit@gmail.com>
2024-08-18 14:03:16 +0530

    transaction screen se chedkhanu

--- lib/src/data/sensors.dart
+++ lib/src/data/sensors.dart
@@ -1,28 +1,24 @@
 import 'package:inventory/src/data/model.dart';
 
-class Sensors{
-
-List<Component> components = [
-  Component(name: 'Ultrasonic (HC-SR04)', boxNo: '', stock: 1),
-  Component(name: 'IR SENSOR (photo diode)', boxNo: '', stock: 17),
-  Component(name: 'PIR sensor', boxNo: '', stock: 1),
-  Component(name: 'Hall effect sensor', boxNo: '', stock: 7),
-  Component(name: 'pressure sensor (MPXV5100DP)', boxNo: '', stock: 2),
-  Component(name: 'LDR sensor', boxNo: '', stock: 2),
-  Component(name: 'Colour sensor', boxNo: '', stock: 1),
-  Component(name: 'APD-9930 proximity sensor', boxNo: '', stock: 2),
-  Component(name: 'IR sensor array', boxNo: '', stock: 1),
-  Component(name: 'Flex sensor', boxNo: '', stock: 4),
-  Component(name: 'Force sensitive sensor', boxNo: '', stock: 1),
-  Component(name: 'Peltier module', boxNo: '', stock: 1),
-  Component(name: 'Doppler radar sensor', boxNo: '', stock: 4),
-  Component(name: 'ECG patches', boxNo: '', stock: 3),
-  Component(name: 'Load Cell (40 kg)', boxNo: '', stock: 1),
-  Component(name: 'Load Cell (5 kg)', boxNo: '', stock: 1),
-  Component(name: 'Range Current Sensor', boxNo: '', stock: 2),
-];
-
-
-
-
+class Sensors {
+  List<Component> components = [
+    Component(name: 'Ultrasonic (HC-SR04)', boxNo: '', stock: 1),
+    Component(name: 'IR SENSOR (photo diode)', boxNo: '', stock: 17),
+    Component(name: 'PIR sensor', boxNo: '', stock: 1),
+    Component(name: 'Hall effect sensor', boxNo: '', stock: 7),
+    Component(name: 'pressure sensor (MPXV5100DP)', boxNo: '', stock: 2),
+    Component(name: 'LDR sensor', boxNo: '', stock: 2),
+    Component(name: 'Colour sensor', boxNo: '', stock: 1),
+    Component(name: 'APD-9930 proximity sensor', boxNo: '', stock: 2),
+    Component(name: 'IR sensor array', boxNo: '', stock: 1),
+    Component(name: 'Flex sensor', boxNo: '', stock: 4),
+    Component(name: 'Force sensitive sensor', boxNo: '', stock: 1),
+    Component(name: 'Peltier module', boxNo: '', stock: 1),
+    Component(name: 'Doppler radar sensor', boxNo: '', stock: 4),
+    Component(name: 'ECG patches', boxNo: '', stock: 3),
+    Component(name: 'Load Cell (40 kg)', boxNo: '', stock: 1),
+    Component(name: 'Load Cell (5 kg)', boxNo: '', stock: 1),
+    Component(name: 'Range Current Sensor', boxNo: '', stock: 2),
+    Component(name: 'Lidar', boxNo: '', stock: 2),
+  ];
 }
--- lib/src/features/main_app/menu_screen/menu_Screen.dart
+++ lib/src/features/main_app/menu_screen/menu_Screen.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:inventory/src/features/main_app/menu_screen/DetailScreen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class fetcheddata{
+class fetcheddata {
+  String MemberName;
+  List<dynamic> packageitems;
+  String issueDate;
+  String? ReturnDate;
+  String SKU;
 
- String MemberName;
- List<dynamic> packageitems;
- String issueDate;
- String? ReturnDate;
- 
-
- fetcheddata({required this.MemberName,required this.packageitems,required this.issueDate,required this.ReturnDate});
-
-
+  fetcheddata(
+      {required this.MemberName,
+      required this.packageitems,
+      required this.issueDate,
+      required this.SKU,
+      required this.ReturnDate});
 }
 
 class MenuScreen extends StatefulWidget {
@@ -30,18 +33,15 @@ class _MenuScreenState extends State<MenuScreen> {
   void fetchpackage() async {
     final fetchedpackage = await supabase.from('Transactions').select();
 
- 
-
-
-    for(var item in fetchedpackage)
-    {
-
-         print(item['package']);
-        fetchedlist.add(
-          fetcheddata(MemberName: item['name'], packageitems: item['package'], issueDate: item['issuedate'],ReturnDate: item['returndate'])
-          
-        );
-        
+    for (var item in fetchedpackage) {
+      print(item['package']);
+      fetchedlist.add(fetcheddata(
+        MemberName: item['name'],
+        packageitems: item['package'],
+        issueDate: item['issuedate'],
+        ReturnDate: item['returndate'],
+        SKU: item['sku'],
+      ));
     }
     print(fetchedlist);
 
@@ -50,7 +50,6 @@ class _MenuScreenState extends State<MenuScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     fetchpackage();
   }
@@ -58,97 +57,107 @@ class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-    Container(
-          height: MediaQuery.of(context).size.height * 1,
-          width: MediaQuery.of(context).size.width * 1,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 154, 210, 255),
-                Color.fromARGB(255, 213, 245, 252),
-                Color.fromARGB(255, 242, 254, 255)
-              ],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-            ),
-          ),
-          
+        body: Container(
+      height: MediaQuery.of(context).size.height * 1,
+      width: MediaQuery.of(context).size.width * 1,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 154, 210, 255),
+            Color.fromARGB(255, 213, 245, 252),
+            Color.fromARGB(255, 242, 254, 255)
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
       child: SingleChildScrollView(
         child: ListView.builder(
-          shrinkWrap: true, // Add this                    
-                        physics: NeverScrollableScrollPhysics(),
-          itemCount: fetchedlist.length,
-          itemBuilder: (ctx,index){
-        
-            final fetcedcomp=fetchedlist[index];
-            final List<String> itemNames=fetcedcomp.packageitems.map<String>(((item)=>item['compname'].toString())).toList();
-          print(itemNames);
-            
-           final item1=itemNames[0];
-           final item2=itemNames.length>1?itemNames[1]:'';
-        
-           
-            return 
-                    Padding(
-         padding: const EdgeInsets.symmetric(
-                                horizontal: 15, vertical: 10),                  child: Container(
-                         width: MediaQuery.of(context).size.width * 0.8,
-                              height: 160,
-                              decoration: BoxDecoration(
-                                  color: Color.fromARGB(39, 5, 168, 244),
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(16),
-                                  )),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                       Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 10, left: 10),
-                                        child: Text(
-                                         '${item1},${item2}...',
-                                          style: GoogleFonts.lato(
-                                              color: Colors.black, fontSize: 20),
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 10, left: 10),
-                                        child: Text(
-                                         'Member:${fetcedcomp.MemberName}',
-                                          style: GoogleFonts.lato(
-                                              color: Colors.black, fontSize: 20),
-                                        ),
-                                      ),
-                                       Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 10, left: 10),
-                                        child: Text(
-                                         'Issued On:${fetcedcomp.issueDate}',
-                                          style: GoogleFonts.lato(
-                                              color: Colors.black, fontSize: 20),
-                                        ),
-                                      ),
-                                        Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 10, left: 10),
-                                        child: Text(
-                                         'Returned On:${fetcedcomp.ReturnDate??'Not Returned'}',
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: fetchedlist.length,
+            itemBuilder: (ctx, index) {
+              final fetcedcomp = fetchedlist[index];
+              final List<String> itemNames = fetcedcomp.packageitems
+                  .map<String>(((item) => item['compname'].toString()))
+                  .toList();
 
-                                          style: GoogleFonts.lato(
-                                              color: Colors.black, fontSize: 20),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
+              final item1 = itemNames[0];
+              final item2 = itemNames.length > 1 ? itemNames[1] : '';
+
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailScreen(
+                        fetchedcomp: fetcedcomp,
                       ),
-                    );
-          }),
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: 180,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(39, 5, 168, 244),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(16),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, left: 10),
+                          child: Text(
+                            '${item1}, ${item2}...',
+                            style: GoogleFonts.lato(
+                                color: Colors.black, fontSize: 20),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, left: 10),
+                          child: Text(
+                            'SKU: ${fetcedcomp.SKU}',
+                            style: GoogleFonts.lato(
+                                color: Colors.black, fontSize: 20),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, left: 10),
+                          child: Text(
+                            'Member: ${fetcedcomp.MemberName}',
+                            style: GoogleFonts.lato(
+                                color: Colors.black, fontSize: 20),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, left: 10),
+                          child: Text(
+                            'Issued On: ${fetcedcomp.issueDate}',
+                            style: GoogleFonts.lato(
+                                color: Colors.black, fontSize: 20),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, left: 10),
+                          child: Text(
+                            'Returned On: ${fetcedcomp.ReturnDate ?? 'Not Returned'}',
+                            style: GoogleFonts.lato(
+                                color: Colors.black, fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }),
       ),
-    
-        )
-      
-    );
+    ));
   }
 }
