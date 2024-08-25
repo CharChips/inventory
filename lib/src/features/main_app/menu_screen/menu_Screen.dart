097974c665b41a105fa2bc511cb122ab6a897919
GazalSG <gazalsgsg1@gmail.com>
2024-08-26 00:18:59 +0530

    Update menu_Screen.dart
    Signed-off-by: GazalSG <gazalsgsg1@gmail.com>

--- lib/src/features/main_app/menu_screen/menu_Screen.dart
+++ lib/src/features/main_app/menu_screen/menu_Screen.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:inventory/src/features/main_app/menu_screen/DetailScreen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class fetcheddata {
-  String MemberName;
-  List<dynamic> packageitems;
+class FetchedData {
+  String memberName;
+  List<dynamic> packageItems;
   String issueDate;
-  String? ReturnDate;
-  String SKU;
+  String? returnDate;
 
-  fetcheddata(
-      {required this.MemberName,
-      required this.packageitems,
-      required this.issueDate,
-      required this.SKU,
-      required this.ReturnDate});
+  FetchedData({
+    required this.memberName,
+    required this.packageItems,
+    required this.issueDate,
+    this.returnDate,
+  });
 }
 
 class MenuScreen extends StatefulWidget {
@@ -28,136 +26,223 @@ class MenuScreen extends StatefulWidget {
 class _MenuScreenState extends State<MenuScreen> {
   final supabase = Supabase.instance.client;
 
-  List<fetcheddata> fetchedlist = [];
+  List<FetchedData> fetchedList = [];
+  List<FetchedData> filteredList = [];
 
-  void fetchpackage() async {
-    final fetchedpackage = await supabase.from('Transactions').select();
+  String? selectedFilter;
+  String searchQuery = '';
 
-    for (var item in fetchedpackage) {
-      print(item['package']);
-      fetchedlist.add(fetcheddata(
-        MemberName: item['name'],
-        packageitems: item['package'],
-        issueDate: item['issuedate'],
-        ReturnDate: item['returndate'],
-        SKU: item['sku'],
-      ));
+  void fetchPackage() async {
+    final fetchedPackage = await supabase.from('Transactions').select();
+    List<FetchedData> tempList = [];
+
+    for (var item in fetchedPackage) {
+      tempList.add(
+        FetchedData(
+          memberName: item['name'],
+          packageItems: item['package'],
+          issueDate: item['issuedate'],
+          returnDate: item['returndate'],
+        ),
+      );
     }
-    print(fetchedlist);
 
-    setState(() {});
+    setState(() {
+      fetchedList = tempList;
+      applyFilters();
+    });
+  }
+
+  void applyFilters() {
+    setState(() {
+      // Filter based on search query and selected filter
+      filteredList = fetchedList.where((data) {
+        // Search in memberName and package items
+        final matchesMemberName = data.memberName.toLowerCase().contains(searchQuery.toLowerCase());
+        final matchesPackageItems = data.packageItems.any((item) =>
+            item['compname'].toString().toLowerCase().contains(searchQuery.toLowerCase()));
+
+        return matchesMemberName || matchesPackageItems;
+      }).toList();
+
+      // Sort the filtered list based on the selected filter
+      switch (selectedFilter) {
+        case 'Member Name':
+          filteredList.sort((a, b) => a.memberName.compareTo(b.memberName));
+          break;
+        case 'Issue Date':
+          filteredList.sort((a, b) => a.issueDate.compareTo(b.issueDate));
+          break;
+        case 'Return Date':
+          filteredList.sort((a, b) => (a.returnDate ?? '').compareTo(b.returnDate ?? ''));
+          break;
+        default:
+          break;
+      }
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    fetchpackage();
+    fetchPackage();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      height: MediaQuery.of(context).size.height * 1,
-      width: MediaQuery.of(context).size.width * 1,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 154, 210, 255),
-            Color.fromARGB(255, 213, 245, 252),
-            Color.fromARGB(255, 242, 254, 255)
-          ],
-          begin: Alignment.bottomCenter,
-          end: Alignment.topCenter,
+      body: Container(
+        height: MediaQuery.of(context).size.height * 1,
+        width: MediaQuery.of(context).size.width * 1,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 154, 210, 255),
+              Color.fromARGB(255, 213, 245, 252),
+              Color.fromARGB(255, 242, 254, 255)
+            ],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
         ),
-      ),
-      child: SingleChildScrollView(
-        child: ListView.builder(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: fetchedlist.length,
-            itemBuilder: (ctx, index) {
-              final fetcedcomp = fetchedlist[index];
-              final List<String> itemNames = fetcedcomp.packageitems
-                  .map<String>(((item) => item['compname'].toString()))
-                  .toList();
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search...',
+                          hintStyle: GoogleFonts.lato(
+                            color: const Color.fromARGB(255, 11, 12, 37),
+                          ),
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
+                        ),
+                        style: GoogleFonts.lato(color: Colors.black),
+                        onChanged: (value) {
+                          searchQuery = value;
+                          applyFilters();
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    DropdownButton<String>(
+                      value: selectedFilter,
+                      hint: Text(
+                        'Filter by',
+                        style: GoogleFonts.lato(
+                          color: const Color.fromARGB(255, 11, 12, 37),
+                        ),
+                      ),
+                      items: [
+                        'Member Name',
+                        'Issue Date',
+                        'Return Date',
+                      ].map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: GoogleFonts.lato(
+                              color: const Color.fromARGB(255, 228, 227, 229),
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        selectedFilter = value;
+                        applyFilters();
+                      },
+                      dropdownColor: Colors.black,
+                      iconEnabledColor:
+                          const Color.fromARGB(255, 11, 12, 37),
+                    ),
+                  ],
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: filteredList.length,
+                itemBuilder: (ctx, index) {
+                  final fetchedComp = filteredList[index];
+                  final List<String> itemNames = fetchedComp.packageItems
+                      .map<String>((item) => item['compname'].toString())
+                      .toList();
+                  final item1 = itemNames.isNotEmpty ? itemNames[0] : '';
+                  final item2 = itemNames.length > 1 ? itemNames[1] : '';
 
-              final item1 = itemNames[0];
-              final item2 = itemNames.length > 1 ? itemNames[1] : '';
-
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailScreen(
-                        fetchedcomp: fetcedcomp,
+                  return Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(39, 5, 168, 244),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 15, left: 15),
+                            child: Text(
+                              '${item1}, ${item2}...',
+                              style: GoogleFonts.lato(
+                                color: Colors.black,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10, left: 15),
+                            child: Text(
+                              'Member: ${fetchedComp.memberName}',
+                              style: GoogleFonts.lato(
+                                color: Colors.black,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10, left: 15),
+                            child: Text(
+                              'Issued On: ${fetchedComp.issueDate}',
+                              style: GoogleFonts.lato(
+                                color: Colors.black,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10, left: 15),
+                            child: Text(
+                              'Returned On: ${fetchedComp.returnDate ?? 'Not Returned'}',
+                              style: GoogleFonts.lato(
+                                color: Colors.black,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   );
                 },
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    height: 180,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(39, 5, 168, 244),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(16),
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10, left: 10),
-                          child: Text(
-                            '${item1}, ${item2}...',
-                            style: GoogleFonts.lato(
-                                color: Colors.black, fontSize: 20),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10, left: 10),
-                          child: Text(
-                            'SKU: ${fetcedcomp.SKU}',
-                            style: GoogleFonts.lato(
-                                color: Colors.black, fontSize: 20),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10, left: 10),
-                          child: Text(
-                            'Member: ${fetcedcomp.MemberName}',
-                            style: GoogleFonts.lato(
-                                color: Colors.black, fontSize: 20),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10, left: 10),
-                          child: Text(
-                            'Issued On: ${fetcedcomp.issueDate}',
-                            style: GoogleFonts.lato(
-                                color: Colors.black, fontSize: 20),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10, left: 10),
-                          child: Text(
-                            'Returned On: ${fetcedcomp.ReturnDate ?? 'Not Returned'}',
-                            style: GoogleFonts.lato(
-                                color: Colors.black, fontSize: 20),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            }),
+              ),
+            ],
+          ),
+        ),
       ),
-    ));
+    );
   }
 }
